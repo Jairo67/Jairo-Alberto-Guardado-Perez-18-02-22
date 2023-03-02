a0476f27d461072d1930122afc2f568c8b7b7565
Jairo67 <125721440+Jairo67@users.noreply.github.com>
2023-03-01 22:34:39 -0600

    Add files via upload

---
+++ Laboratorio1_Jairo_Alberto_Guaradado_Perez/Laboratorio1_Jairo_Alberto_Guaradado_Perez/Negocio/Clspersonas.cs
@@ -0,0 +1,28 @@
+﻿using Laboratorio1_Jairo_Alberto_Guaradado_Perez.Entidades;
+using Laboratorio1_Jairo_Alberto_Guaradado_Perez.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1_Jairo_Alberto_Guaradado_Perez.Negocio
+{
+    public class Clspersonas
+    {
+        List<Personas> Per = new List<Personas>();
+
+        public void AgregarPersonas(Personas per)
+        {
+
+            Per.Add(per);
+
+        }
+
+        public List<Personas> ListarPersonas()
+        {
+
+            return Per;
+        }
+    }
+}
