10e11472a1dc1df5b4c0b052586b67fe862996a5
Jairo67 <125721440+Jairo67@users.noreply.github.com>
2023-04-12 11:47:10 -0600

    Add files via upload

---
+++ Laboratorio2/Notas/Notas/DAO/NotaDao.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notas.DAO
+{
+    public class NotaDao
+    {
+    
+            private SqlConnection conexion;
+
+            public NotaDao()
+            {
+                string cadenaConexion = "Data Source=(local);Initial Catalog=NotaEstudiante;Integrated Security=True";
+                this.conexion = new SqlConnection(cadenaConexion);
+            }
+
+            public void GuardarNotas(string nombreMateria, string nombreEstudiante, decimal lab1, decimal parcial1, decimal lab2, decimal parcial2, decimal lab3, decimal parcial3, decimal resultadoFinal)
+            {
+                try
+                {
+                    this.conexion.Open();
+                    SqlCommand comando = new SqlCommand("INSERT INTO Nota (NombreMateria, NombreEstudiante, Lab1, Parcial1, Lab2, Parcial2, Lab3, Parcial3, Resultado) VALUES (@NombreMateria, @NombreEstudiante, @Lab1, @Parcial1, @Lab2, @Parcial2, @Lab3, @Parcial3, @Resultado)", this.conexion);
+                    comando.Parameters.AddWithValue("@NombreMateria", nombreMateria);
+                    comando.Parameters.AddWithValue("@NombreEstudiante", nombreEstudiante);
+                    comando.Parameters.AddWithValue("@Lab1", lab1);
+                    comando.Parameters.AddWithValue("@Parcial1", parcial1);
+                    comando.Parameters.AddWithValue("@Lab2", lab2);
+                    comando.Parameters.AddWithValue("@Parcial2", parcial2);
+                    comando.Parameters.AddWithValue("@Lab3", lab3);
+                    comando.Parameters.AddWithValue("@Parcial3", parcial3);
+                    comando.Parameters.AddWithValue("@Resultado", resultadoFinal);
+                    comando.ExecuteNonQuery();
+                    Console.WriteLine("Notas guardadas exitosamente.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al guardar las notas: " + ex.Message);
+                }
+                finally
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            public decimal CalcularResultadoFinal(decimal lab1, decimal parcial1, decimal lab2, decimal parcial2, decimal lab3, decimal parcial3)
+            {
+                decimal resultadoPeriodo1 = lab1 * 0.4m + parcial1 * 0.6m;
+                decimal resultadoPeriodo2 = lab2 * 0.4m + parcial2 * 0.6m;
+                decimal resultadoPeriodo3 = lab3 * 0.4m + parcial3 * 0.6m;
+
+                decimal resultadoFinal = (resultadoPeriodo1 + resultadoPeriodo2 + resultadoPeriodo3) / 3;
+
+                return resultadoFinal;
+            }
+        
+    }
+
+}
+
---
+++ Laboratorio2/Notas/Notas/Vista/Programa.cs
@@ -0,0 +1,77 @@
+﻿using Notas.DAO;
+using Notas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notas.Vistas
+{
+    public class Vista
+    {
+        private Notum modelo;
+        private NotaDao dao;
+
+        public Vista()
+        {
+            this.modelo = new Notum();
+            this.dao = new NotaDao();
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("1. Calcular notas");
+            Console.WriteLine("2. Listar notas");
+            Console.WriteLine("3. Salir");
+            Console.Write("Seleccione una opción: ");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    CalcularNotas();
+                    break;
+                case 2:
+                    ListarNotas();
+                    break;
+                case 3:
+                    Console.WriteLine("¡Hasta luego!");
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida, seleccione de nuevo.");
+                    Menu();
+                    break;
+            }
+        }
+
+        public void CalcularNotas()
+        {
+            Console.WriteLine("Ingrese el nombre de la materia:");
+            string materia = Console.ReadLine();
+            Console.WriteLine("Ingrese el nombre del estudiante:");
+            string estudiante = Console.ReadLine();
+            Console.WriteLine("Ingrese la nota del primer laboratorio:");
+            double lab1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del primer parcial:");
+            double parcial1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del segundo laboratorio:");
+            double parcial2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del segundo parcial:");
+            double lab2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del tercer laboratorio:");
+            double lab3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del tercer parcial:");
+            double parcial3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"La nota definitiva del estudiante {estudiante} en la materia {materia} es {definitiva}");
+        }
+
+        public void ListarNotas()
+        {
+            foreach (var nota in dao.ObtenerNotas())
+            {
+                Console.WriteLine($"Materia: {nota.Materia} - Estudiante: {nota.Estudiante} - Parcial 1: {nota.Parcial1} - Parcial 2: {nota.Parcial2} - Definitiva: {nota.Definitiva}");
+            }
+        }
+    }
+}
