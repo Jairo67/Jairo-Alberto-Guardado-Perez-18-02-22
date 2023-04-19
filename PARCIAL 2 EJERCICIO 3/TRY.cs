f5b47d6327a32b51203af90615c50051894547f4
Jairo67 <125721440+Jairo67@users.noreply.github.com>
2023-04-18 21:06:00 -0600

    Add files via upload

---
+++ PARCIAL 2 EJERCICIO 3/TRY.cs
@@ -0,0 +1,13 @@
+﻿int dato1 = 16;
+string dato2 = "25kg";
+int resultado;
+
+try
+{
+    resultado = dato1 + Convert.ToInt32(dato2);
+    Console.WriteLine(resultado);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
